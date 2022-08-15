0f264c8210333c16deed5b7e85e34b07e8583cfc
Benjamin Ung <Benjaminung@cpp.edu>
2022-08-15 14:42:38 -0700

    finished configuring django set up, load in env to run

---
+++ main.py
@@ -0,0 +1,10 @@
+#use py instead of python --version
+#py -m pip install ____
+#to enter the env (enter in command line): env\scripts\activate [note the (env) at the beginning of line]
+#to exit the env: deactivate 
+'''
+(env) PS C:\Users\benja\GIT\Django> cd .\studybud\
+(env) PS C:\Users\benja\GIT\Django\studybud> python manage.py runserver
+
+'''
+print("hello world!")
