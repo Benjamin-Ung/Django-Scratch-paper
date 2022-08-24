1aa33e27d2158b79e8d9165a27b229e1bb8c5721
Benjamin Ung <Benjaminung@cpp.edu>
2022-08-24 14:54:11 -0700

    initiated migration and learned models/admin panel

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    #host = 
+    #topic =
+    name = models.CharField(max_length=200)
+
+    #database can have a null value / form can be blank as well
+    #can add room without description 
+    description = models.TextField(null = True, blank = True)
+
+    #participants = 
+
+    #take a time stamp when updated
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
