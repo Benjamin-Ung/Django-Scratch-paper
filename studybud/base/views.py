a8dd6bd1f36270d0b2c1747d3a5c6edf93ab1e39
Benjamin Ung <Benjaminung@cpp.edu>
2022-08-31 15:43:19 -0700

    created basic user profiles

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -105,6 +105,17 @@ def room(request, pk):
     context = {'room': room, 'room_messages': room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
 
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+
+    context = {'user':user, 'rooms':rooms, 'room_messages':room_messages, 'topics': topics}
+    return render(request, 'base/profile.html', context)
+
+
 @login_required(login_url = 'login') #requires you to be logged in to create room / else be redirected
 def createRoom(request):
     form = RoomForm()
@@ -148,7 +159,6 @@ def deleteRoom(request, pk):
         return redirect('home')
         
     return render(request, 'base/delete.html',{'obj':room})
-
 
 @login_required(login_url = 'login')
 def deleteMessage(request, pk):
