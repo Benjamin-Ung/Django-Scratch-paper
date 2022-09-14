d8af1ef154a55421a7a8fdac6ac9943428b800a9
Benjamin Ung <Benjaminung@cpp.edu>
2022-09-14 14:00:39 -0700

    cleaned up user update page

--- studybud/base/forms.py
+++ studybud/base/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm):
     class Meta:
@@ -7,3 +8,9 @@ class RoomForm(ModelForm):
         fields = '__all__'
         #this line removes the fields in models/room from being displayed
         exclude = ['host', 'participants']
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username','email']
+
